160af125f8f25c67f3725bc61a976db5d1b2523e
Panagiotis Tsionis <69398519+ptsionis@users.noreply.github.com>
2024-01-17 23:26:22 +0200

    changed spotify list

--- src/components/SpotifyPlayer/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer/SpotifyPlayer.jsx
@@ -8,9 +8,11 @@ export default function SpotifyPlayer() {
     <Fade duration={1500} direction="up">
       <iframe
         className="spotify-embed mt-3"
-        src="https://open.spotify.com/embed/playlist/0Sdb5BILPZauc1bDm74UPT?utm_source=generator&theme=0"
+        src="https://open.spotify.com/embed/playlist/6aTz7YX8U0OmNImQfT98g4?utm_source=generator&theme=0"
         width="100%"
-        height="152px"
+        height="352"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
       ></iframe>
     </Fade>
   );
