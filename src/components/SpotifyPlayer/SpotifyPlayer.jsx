0b4a378a28ea23ec5c360c911c20f1be460159f5
Panagiotis Tsionis <69398519+ptsionis@users.noreply.github.com>
2024-01-26 18:43:39 +0200

    removed fade from spotify

--- src/components/SpotifyPlayer/SpotifyPlayer.jsx
+++ src/components/SpotifyPlayer/SpotifyPlayer.jsx
@@ -5,15 +5,13 @@ import "./SpotifyPlayer.css";
 
 export default function SpotifyPlayer() {
   return (
-    <Fade duration={1500} direction="up">
-      <iframe
-        className="spotify-embed mt-3"
-        src="https://open.spotify.com/embed/playlist/6aTz7YX8U0OmNImQfT98g4?utm_source=generator&theme=0"
-        width="100%"
-        height="352"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
-    </Fade>
+    <iframe
+      className="spotify-embed mt-4"
+      src="https://open.spotify.com/embed/playlist/6aTz7YX8U0OmNImQfT98g4?utm_source=generator&theme=0"
+      width="90%"
+      height="152"
+      allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+      loading="lazy"
+    ></iframe>
   );
 }
