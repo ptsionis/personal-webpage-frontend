14b65a4b19732d67c66ec98d5e02c694c13933b6
Panagiotis Tsionis <69398519+ptsionis@users.noreply.github.com>
2024-01-28 00:56:35 +0200

    added palette on error page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -5,11 +5,13 @@ import Starfield from "../../components/Starfield/Starfield";
 import SpotifyPlayer from "../../components/SpotifyPlayer/SpotifyPlayer";
 
 import "./NotFound.css";
+import Palette from "../../components/Palette/Palette";
 
 const NotFound = () => {
   return (
     <div className="wrapper">
       <Starfield />
+      <Palette />
       <main className="container d-flex flex-column justify-content-center align-items-center text-center">
         <h1>404</h1>
         <Link className="back-to-home m-4 p-3 text-decoration-none" to={"/"}>
